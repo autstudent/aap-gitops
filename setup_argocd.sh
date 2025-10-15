6f8c3bb3b01e6700bad6722f1b989fc5c1f20bf5
Asier Cidon <asier.cidon@gmail.com>
2025-10-15 11:49:21 +0200

    first commit

---
+++ setup_argocd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+##
+# Setup Openshift Environment
+##
+
+echo "Setting Up Jump App stuff in Openshift..."
+
+# Install Operators
+echo "Installing ArgoCD operator..."
+helm template ./charts/ocp-solutions -f ./files/values-install-argocd.yaml --debug | oc apply -f -
+sleep 60
+
+# Apply chart template
+echo "Creating ArgoCD Server and Bootstrap the Openshift Cluster"
+helm template ./charts/argocd -f ./files/values-config-argocd.yaml --debug --namespace openshift-gitops | oc apply -f - -n openshift-gitops
+
+# Notifications
+read -p "Openshift Cluster bootstrap finished"
